c6d27644c97efcd48f1ce3ef9218008b583e9ffe
jess <jiayu1551@gmail.com>
2021-08-06 16:03:50 +0800

    adjust home layout display, and tab switch page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <v-app>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/bmi">BMI</router-link> |
-      <router-link to="/todo">TodoList</router-link>
-    </div>
-    <router-view/>
+    <v-app-bar app>
+      <v-avatar class="mr-10">
+        <img src="@/assets/logo.svg">
+      </v-avatar>
+      <v-tabs
+        class="ml-n9"
+      >
+        <v-tab to="/">Home</v-tab>
+        <v-tab to="/bmi">BMI Calculator</v-tab>
+        <v-tab to="/todo">Todo List</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
@@ -15,25 +24,12 @@
 
 <style>
 #app {
-  width: 600px;
+  max-width: 720px;
   margin: 60px auto 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: Roboto, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #1976d2;
 }
 </style>
